748947f9877e935b95d36fa4116f7955dbc84178
Pandazki <onlrrr@gmail.com>
2026-04-24 15:26:08 +0800

    feat(site): card block kit (PillarGrid/Pillar/KV/Formula/Quote/Caption/Chip/Divider/Stack)

---
+++ site/src/components/card/blocks/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Block kit for OneShotCard MDX bodies.  Import individually in MDX:
+ *   import Pillar from '~/components/card/blocks/Pillar.astro';
+ * or auto-imported via MDX components provider (see docs/.../exemplars).
+ */
+export {};
